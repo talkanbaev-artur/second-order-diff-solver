991fd969bdfd5a25eb0853b50d2cf90fc004e82e
Talkanbaev Artur <talkanbaev_a@auca.kg>
2022-10-13 11:57:49 +0600

    Ode solve v0.2

--- src/backend/logic/algorithms/ode.go
+++ src/backend/logic/algorithms/ode.go
@@ -46,16 +46,16 @@ func (s *solver) Precalc() error {
 	n, h, eps := s.Inputs.GridSize+1, 1.0/float64(s.Inputs.GridSize), s.Inputs.EpsilonParam
 	var a, b, c, f = make([]float64, n), make([]float64, n), make([]float64, n), make([]float64, n)
 	a[0], c[n-1] = 0, 0
-	b[0], c[0], f[0] = s.Original.Xi1+s.Original.Eta1*eps/h, s.Original.Eta1*eps/h*-1, s.Original.Phi1(eps)
-	a[n-1], b[n-1], f[n-1] = s.Original.Eta2*eps/h*-1, s.Original.Xi2+s.Original.Eta2*eps/h, s.Original.Phi2(eps)
+	b[0], c[0], f[0] = s.Original.Xi1+s.Original.Eta1*eps/h, s.Original.Eta1*eps/h, s.Original.Phi1(eps)
+	a[n-1], b[n-1], f[n-1] = s.Original.Eta2*eps/h, s.Original.Xi2+s.Original.Eta2*eps/h, s.Original.Phi2(eps)
 	for i := 1; i < n-1; i++ {
 		f[i] = s.Original.F(mesh[i])
 		a_o, b_o := s.Original.A(mesh[i]), s.Original.B(mesh[i])
 		rVal := a_o * h / (2 * eps)
 		eVal, hSq := 2*eps*visc(rVal), math.Pow(h, 2)
-		a[i] = (a_o*h + eVal) / 2 * hSq
+		a[i] = (a_o*h - eVal) / (-2 * hSq)
 		b[i] = -1 * (eVal/hSq + b_o)
-		c[i] = (eVal + a_o*h) / (-2 * hSq)
+		c[i] = (eVal + a_o*h) / (2 * hSq)
 	}
 	s.a, s.b, s.c, s.f = a, b, c, f
 	return nil
